d73b048dceaa948b9c887ce544275ab2eaabda43
sj0rs1 <sjburhennemail@gmail.com>
2021-01-24 14:19:14 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,52 +1,64 @@
+getgenv().looped = false
 workspace:WaitForChild("Map")
+local Enabled = true
 local Players = game:GetService("Players")
 local Debris = game:GetService("Debris")
-local UserInputService = game:GetService("UserInputService")
 local LocalPlayer = Players.LocalPlayer
-local client = getsenv(LocalPlayer.PlayerGui.Client)
 local mapid = getgenv().modelid
-local stom = client.setcharacter
-
-local map = game:GetObjects("rbxassetid://"..mapid)[1]
-map.Name = "custommap"
-map.Parent = workspace
-map:MoveTo(Vector3.new(0,3,0))
+local map
 
 function updateMap(o)
-    local T,CT = o:WaitForChild("TSpawns"),o:WaitForChild("CTSpawns")
-    for i,v in next, T:GetChildren() do
-        v.Position = map.spawns.T:GetChildren()[math.random(1,#map.spawns.T:GetChildren())].Position
-    end
-    for i,v in next, CT:GetChildren() do
-        v.Position = map.spawns.CT:GetChildren()[math.random(1,#map.spawns.CT:GetChildren())].Position
+    if not Enabled then return end
+    if map then map:Destroy(); map = nil end
+    map = game:GetObjects("rbxassetid://"..mapid)[1]
+    map.Name = "custommap"
+    map.Parent = workspace
+    map:MoveTo(Vector3.new(0,1500,0))
+    for i,v in next, map:GetDescendants() do
+        pcall(function()
+            v.Anchored = true
+        end)
     end
 end
-updateMap(workspace.Map)
-LocalPlayer.CharacterAdded:connect(function(char)
-    if LocalPlayer.Status.Team.Value == "Spectator" then repeat wait() until LocalPlayer.Status.Team.Value ~= "Spectator" end
-    char:WaitForChild("HumanoidRootPart")
-    wait(0.8)
-    local folder = workspace.Map[LocalPlayer.Status.Team.Value .. "Spawns"]
-    LocalPlayer.Character:SetPrimaryPartCFrame(folder:GetChildren()[math.random(1, #folder:GetChildren())].CFrame * CFrame.new(0, 4, 0))
-    wait(0.8)
-    stom()
-end)
 workspace.ChildAdded:connect(function(th)
     if th.Name == "Map" then updateMap(th) end
 end)
-client.setcharacter = function()end
-UserInputService.InputBegan:connect(function(k)
-    if k.KeyCode == Enum.KeyCode.B and LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("HumanoidRootPart") and not client.Buymenuframe.Visible then
-        client.Buymenuframe.Visible = true
-        client.BuyMenuOpen = true
-    end
-end)
-while wait() do
+
+getgenv().looped = true
+updateMap(workspace.Map)
+while getgenv().looped do wait()
     if map:FindFirstChild("settings") then
         if map.settings:FindFirstChild("gravity") then
             workspace.Gravity = map.settings.gravity.Value
         else
             workspace.Gravity = 80
         end
+        if map.settings:FindFirstChild("time") then
+            game.Lighting.TimeOfDay = map.settings.time.Value
+        end
+        if map.settings:FindFirstChild("skybox") then
+            if game.Lighting:FindFirstChild("skybox") then
+                game.Lighting.skybox:Destroy()
+            end
+            if game.Lighting:FindFirstChild("SunRays") then
+                game.Lighting.SunRays.Enabled = false
+            end
+            local skybox = map.settings.skybox:Clone()
+            skybox.Parent = game.Lighting
+        end
+    end
+    if map and LocalPlayer.Character and LocalPlayer.Character.PrimaryPart then
+        local folder = map.spawns[LocalPlayer.Status.Team.Value]
+        if folder then
+            local ypos = LocalPlayer.Character.PrimaryPart.Position.Y
+            local spawnpos = folder:GetChildren()[1].Position.Y
+            if spawnpos - 50 > ypos then
+                LocalPlayer.Character:SetPrimaryPartCFrame(folder:GetChildren()[math.random(1, #folder:GetChildren())].CFrame * CFrame.new(0,3,0))
+            end
+        end
     end
 end
+
+Enabled = false
+workspace.Gravity = 80
+if map then map:Destroy(); map = nil end
