3bc7e5af4491e845772a8a9c2539232e8b54cdb8
sj0rs1 <sjburhennemail@gmail.com>
2021-01-24 14:25:46 +0100

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,2 +1,2 @@
-getgenv().modelid = "6292702783" -- make sure ur map is uploaded with copying ENABLED.
+getgenv().modelid = "6292720589" -- make sure ur map is uploaded with copying ENABLED.
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sj0rs1/custom-maps/main/main.lua",true))()
